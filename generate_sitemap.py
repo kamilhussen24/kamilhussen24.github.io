e61486af4631ac21c9862a69c7fac36d2057efe8
Kamil Hussen <76224565+kamilhussen24@users.noreply.github.com>
2025-04-03 03:17:40 +0600

    Update generate_sitemap.py

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -1,44 +1,38 @@
 import os
-import requests
-from datetime import datetime
 
-# আপনার ওয়েবসাইটের মূল লিংক
-SITE_URL = "https://kamilhussen24.github.io"
-
-# সাইটম্যাপ ফাইলের নাম
 SITEMAP_FILE = "sitemap.xml"
 
-# নতুন পোস্ট লিস্ট
-NEW_POSTS = [
-    "/blog/post/online-scammer-identify-bangla",
-    "/about-me",
-    "/privacy-policy",
-    "/blog",
-    "/terms-of-service"
+BASE_URL = "https://kamilhussen24.github.io"
+
+# আপনার HTML ফাইল ও ব্লগ পোস্টের তালিকা
+pages = [
+    "",
+    "about-me",
+    "blog",
+    "blog/post/online-scammer-identify-bangla",
+    "privacy-policy",
+    "terms-of-service"
 ]
 
-# বর্তমান তারিখ ISO 8601 ফরম্যাটে
-current_date = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S+00:00")
-
-# নতুন সাইটম্যাপ XML তৈরি করা
-sitemap_content = f'''<?xml version="1.0" encoding="UTF-8"?>
+# XML ফরম্যাট তৈরি করা
+sitemap_content = '''<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 '''
-
-# প্রতিটি লিংক অ্যাড করা
-for post in NEW_POSTS:
-    sitemap_content += f'''    <url>
-        <loc>{SITE_URL}{post}</loc>
-        <lastmod>{current_date}</lastmod>
-        <priority>0.80</priority>
+for page in pages:
+    url = f"{BASE_URL}/{page}" if page else BASE_URL
+    sitemap_content += f"""
+    <url>
+        <loc>{url}</loc>
+        <lastmod>{os.popen('date -u +"%Y-%m-%dT%H:%M:%SZ"').read().strip()}</lastmod>
+        <priority>0.8</priority>
         <changefreq>weekly</changefreq>
-    </url>\n'''
+    </url>
+    """
 
-# সাইটম্যাপ শেষ করা
-sitemap_content += "</urlset>"
+sitemap_content += "\n</urlset>"
 
-# ফাইল সেভ করা
-with open(SITEMAP_FILE, "w", encoding="utf-8") as file:
-    file.write(sitemap_content)
+# ফাইল তৈরি এবং সংরক্ষণ
+with open(SITEMAP_FILE, "w", encoding="utf-8") as f:
+    f.write(sitemap_content)
 
-print(f"✅ {SITEMAP_FILE} সফলভাবে আপডেট হয়েছে!")
+print("✅ Sitemap.xml তৈরি হয়েছে!")
