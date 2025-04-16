a5a538f2525d49741471ccaeed44077051c4b010
Kamil Hussen <76224565+kamilhussen24@users.noreply.github.com>
2025-04-16 22:51:14 +0600

    Update click-scroll.js

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -1,29 +1,32 @@
+//jquery-click-scroll
+//by syamsul'isul' Arifin (Updated by Kamil & ChatGPT)
+
 var sectionArray = [1, 2, 3, 4, 5];
 
-$.each(sectionArray, function(index, value){
-    
-    // Scroll Event
-    $(document).scroll(function(){
-        var offsetSection = $('#section_' + value).offset().top - 90;
-        var docScroll = $(document).scrollTop();
-        var docScroll1 = docScroll + 1;
+// SCROLL EVENT
+$(document).scroll(function () {
+    var docScroll = $(document).scrollTop() + 1;
 
-        if ( docScroll1 >= offsetSection ){
+    $.each(sectionArray, function (index, value) {
+        var offsetSection = $('#section_' + value).offset().top - 90;
+
+        if (docScroll >= offsetSection) {
             $('.navbar-nav .nav-item .nav-link').removeClass('active');
-            $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
-            $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-            $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+            $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
+            $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active').removeClass('inactive');
         }
     });
+});
 
-    // Click Event (শুধু যদি section থাকে, বাইরের লিংকে নয়)
-    var link = $('.click-scroll').eq(index);
-    var href = link.attr('href');
+// CLICK EVENT
+$.each(sectionArray, function (index, value) {
+    var selector = $('.click-scroll').eq(index);
+    var href = selector.attr('href');
 
     if (href && href.startsWith('#section_')) {
-        link.click(function(e){
+        selector.click(function (e) {
             e.preventDefault();
-            var offsetClick = $('#section_' + value).offset().top - 90;
+            var offsetClick = $('#' + 'section_' + value).offset().top - 90;
             $('html, body').animate({
                 'scrollTop': offsetClick
             }, 300);
@@ -31,8 +34,8 @@ $.each(sectionArray, function(index, value){
     }
 });
 
-$(document).ready(function(){
-    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
-    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
-    $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
+// INITIAL ACTIVE CLASS
+$(document).ready(function () {
+    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
+    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active').removeClass('inactive');
 });
