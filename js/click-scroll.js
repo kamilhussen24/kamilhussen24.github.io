32559870c06c098ec6ce33fdcb6f5fb98453b813
Kamil Hussen <76224565+kamilhussen24@users.noreply.github.com>
2025-04-16 22:45:51 +0600

    Update click-scroll.js

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -1,33 +1,34 @@
-//jquery-click-scroll
-//by syamsul'isul' Arifin
-
 var sectionArray = [1, 2, 3, 4, 5];
 
 $.each(sectionArray, function(index, value){
-          
-     $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 90;
-         var docScroll = $(document).scrollTop();
-         var docScroll1 = docScroll + 1;
-         
-        
-         if ( docScroll1 >= offsetSection ){
-             $('.navbar-nav .nav-item .nav-link').removeClass('active');
-             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
-             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
-         }
-         
-     });
     
-    $('.click-scroll').eq(index).click(function(e){
-        var offsetClick = $('#' + 'section_' + value).offset().top - 90;
-        e.preventDefault();
-        $('html, body').animate({
-            'scrollTop':offsetClick
-        }, 300)
+    // Scroll Event
+    $(document).scroll(function(){
+        var offsetSection = $('#section_' + value).offset().top - 90;
+        var docScroll = $(document).scrollTop();
+        var docScroll1 = docScroll + 1;
+
+        if ( docScroll1 >= offsetSection ){
+            $('.navbar-nav .nav-item .nav-link').removeClass('active');
+            $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
+            $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+            $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+        }
     });
-    
+
+    // Click Event (শুধু যদি section থাকে, বাইরের লিংকে নয়)
+    var link = $('.click-scroll').eq(index);
+    var href = link.attr('href');
+
+    if (href && href.startsWith('#section_')) {
+        link.click(function(e){
+            e.preventDefault();
+            var offsetClick = $('#section_' + value).offset().top - 90;
+            $('html, body').animate({
+                'scrollTop': offsetClick
+            }, 300);
+        });
+    }
 });
 
 $(document).ready(function(){
